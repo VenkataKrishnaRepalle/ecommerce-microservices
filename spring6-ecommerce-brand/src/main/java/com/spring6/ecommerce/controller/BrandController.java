9fa7d4d904118122859ebf514b20dcbe1ee582c5
pandimunees <pandimunees@gmail.com>
2023-05-16 16:30:36 +0530

    initial commit

---
+++ spring6-ecommerce-brand/src/main/java/com/spring6/ecommerce/controller/BrandController.java
@@ -0,0 +1,12 @@
+package com.spring6.ecommerce.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BrandController {
+    @GetMapping("hi")
+    public String hello() {
+        return "Hello";
+    }
+}
