659618084929f26508f55f79b2802f3d8fde1a96
praja <pandimunees@gmail.com>
2023-05-17 17:03:41 +0530

    category issue resolved

--- spring6-ecommerce-category/src/test/java/com/spring6/ecommerce/Spring6EcommerceCategoryApplicationTests.java
+++ spring6-ecommerce-category/src/test/java/com/spring6/ecommerce/Spring6EcommerceCategoryApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Spring6EcommerceCategoryApplication {
+class Spring6EcommerceCategoryApplicationTests {
 
 	@Test
 	void contextLoads() {
