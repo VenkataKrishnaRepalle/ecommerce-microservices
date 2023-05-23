981b5b285d7d06be6d46de8fe6cee48a2e9f3a0e
bbaruati <bhaskarbaruati535@gmail.com>
2023-05-23 16:03:12 +0530

    category firstApi done

---
+++ spring6-ecommerce-category/src/main/java/com/spring6/ecommerce/service/SubCategoryService.java
@@ -0,0 +1,9 @@
+package com.spring6.ecommerce.service;
+
+import com.spring6.ecommerce.dto.SubCategoryDto;
+
+import java.util.List;
+
+public interface SubCategoryService {
+    List<SubCategoryDto> listAll();
+}
