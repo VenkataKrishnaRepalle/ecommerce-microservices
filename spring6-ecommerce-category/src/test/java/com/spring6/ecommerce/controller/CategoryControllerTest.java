e4b484ab99cd465281076b805d8f1a55a7513f39
pandimunees <pandimunees@gmail.com>
2023-05-24 11:26:01 +0530

    .idea removed

---
+++ spring6-ecommerce-category/src/test/java/com/spring6/ecommerce/controller/CategoryControllerTest.java
@@ -0,0 +1,12 @@
+package com.spring6.ecommerce.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryControllerTest {
+
+    @Test
+    void listAll() {
+    }
+}
---
+++ spring6-ecommerce-category/src/test/java/com/spring6/ecommerce/service/CategoryServiceTest.java
@@ -0,0 +1,12 @@
+package com.spring6.ecommerce.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryServiceTest {
+
+    @Test
+    void listAll() {
+    }
+}
