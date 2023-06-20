ca5828ca3ed0bd1616c8fd41d58cb0d1f8dacd08
INE12399542 <VenkataKrishna.Repalle@tesco.com>
2023-06-20 13:41:21 +0530

    Spring6-customer-registration module created

---
+++ spring6-customer-registration/src/main/java/com/spring6/spring6customerregistration/Spring6CustomerRegistrationApplication.java
@@ -0,0 +1,14 @@
+package com.spring6.spring6customerregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6CustomerRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6CustomerRegistrationApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ spring6-customer-registration/src/test/java/com/spring6/spring6customerregistration/Spring6CustomerRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring6.spring6customerregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6CustomerRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
