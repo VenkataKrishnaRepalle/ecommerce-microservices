57f5aa6048ff831911d53393da95eadb800cacfd
bbaruati <bhaskarbaruati535@gmail.com>
2023-12-14 16:15:29 +0530

    refactored category service

--- category-service/src/main/java/com/pm/spring/ema/category/CategoryApplication.java
+++ category-service/src/main/java/com/pm/spring/ema/category/CategoryApplication.java
@@ -2,7 +2,9 @@ package com.pm.spring.ema.category;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "com.pm.spring.ema")
 @SpringBootApplication
 public class CategoryApplication {
 
