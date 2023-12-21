ccfdd0aa512e2c95696d4fc83be534bc12273304
repalle venkata krishna <rvkrishna13052001@gmail.com>
2023-12-21 15:53:55 +0530

    Address Service yml updated

---
+++ address-service/src/main/java/com/pm/spring/ema/shipping/exception/ErrorResponse.java
@@ -0,0 +1,16 @@
+package com.pm.spring.ema.shipping.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private List<ErrorFormat> errors;
+}
