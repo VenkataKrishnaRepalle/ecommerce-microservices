88a2244141bcc3766534b58038a943b6ab27579d
pandimuneeswaranraja <pandimuneeswaranraja@gmail.com>
2023-12-28 17:47:58 +0530

    iam

--- auth-service/src/main/java/com/pm/spring/ema/auth/service/common/model/dao/AuditDao.java
+++
@@ -1,8 +0,0 @@
-package com.pm.spring.ema.auth.service.common.model.dao;
-
-import java.util.List;
-import java.util.UUID;
-
-public interface AuditDao {
-    List<Object[]> getAuditRecords(Class<?> entityClass, UUID entityId);
-}
--- auth-service/src/main/java/com/pm/spring/ema/auth/service/common/model/enums/TokenType.java
+++
@@ -1,5 +0,0 @@
-package com.pm.spring.ema.auth.service.common.model.enums;
-
-public enum TokenType {
-    BEARER
-}
